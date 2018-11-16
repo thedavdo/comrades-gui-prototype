a0cfca8de36f4db3665a4c11bf54123242abcb5d
thedavdo <david.baker542@gmail.com>
2018-11-16 17:52:20 -0500

    Implemented EngineCommand and ResponseListener
    Updated to use new the new EngineCommand and ResponseListener classes. Corrected behavior to wait for engine process to be ready before sending additional commands. added more logInfo statements to be more verbose.

--- ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessEngine.java
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessEngine.java
@@ -1,22 +1,89 @@
 package edu.purdue.comradesgui.javafx;
 
+import javafx.concurrent.Task;
+
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.List;
 
 public class ChessEngine {
 
 
-	private Process fileProcess;
+	//private Process fileProcess;
 	private BufferedReader bufReader;
 	private PrintWriter bufWriter;
 
 	private StringBuffer logBuffer;
 
+	private List<ResponseListener> responseListeners;
+	private List<EngineCommand> pushCmdList;
+
+	private String engineName;
+	private String engineAuthor;
+	private List<String> rawOptions;
+
+	private boolean hasLoaded = false;
+
 	private boolean shouldLog = true;
+	private boolean isReady = false;
+	private boolean waitingForReady = false;
 
 	public ChessEngine() {
+
 		logBuffer = new StringBuffer();
+		rawOptions = new ArrayList<>();
+
+		responseListeners = new ArrayList<>();
+
+		pushCmdList = new ArrayList<>();
+
+		engineName = "nil";
+
+		initListener();
+	}
+
+	private void initListener() {
+
+		addResponseListener((cmd) -> {
+			//Listen for Response from 'isready'
+			if(cmd.equals("readyok")) {
+				logInfo("eng < Ready");
+				isReady = true;
+				waitingForReady = false;
+				return true;
+			}
+
+		//Listen for last Response from 'uci'
+			if (cmd.equals("uciok")) {
+				logInfo("UCI Ready.");
+				return true;
+			}
+
+		//Listen for one of the Responses from 'uci'
+			if (cmd.startsWith("id name")) {
+				engineName = cmd.substring(8);
+				logInfo("Engine Name: " + engineName);
+				return true;
+			}
+
+		//Listen for one of the Responses from 'uci'
+			if (cmd.startsWith("id author")) {
+				engineAuthor = cmd.substring(10);
+				logInfo("Author(s): " + engineAuthor);
+				return true;
+			}
+
+		//Listen for one of the Responses from 'uci'
+			if(cmd.startsWith("option")) {
+				rawOptions.add(cmd);
+				logInfo("Option Raw: " + cmd);
+				return true;
+			}
+			return false;
+		});
 	}
 
 	public void setLogging(boolean shouldLog) {
@@ -31,14 +98,24 @@ public class ChessEngine {
 		return logBuffer;
 	}
 
+	public void addResponseListener(ResponseListener rl) {
+		responseListeners.add(rl);
+	}
+
+	public boolean hasLoaded() {
+		return hasLoaded;
+	}
+
 	public void loadFromPath(String path) {
 
 		try {
 			logInfo("Attempting to load from path: " + path);
-			fileProcess = Runtime.getRuntime().exec(path);
+			Process fileProcess = Runtime.getRuntime().exec(path);
 
 			bufReader = new BufferedReader(new InputStreamReader(fileProcess.getInputStream()));
 			bufWriter = new PrintWriter(new OutputStreamWriter(fileProcess.getOutputStream()));
+
+			hasLoaded = true;
 
 			logInfo("Path loaded successfully");
 
@@ -54,12 +131,101 @@ public class ChessEngine {
 
 		logInfo("Initializing Engine...");
 
-		this.sendCommand("ici");
-		this.flushWriter();
-		this.readResponse();
+		Task readerLoop = new Task() {
+			@Override
+			protected Void call() {
+
+				boolean runLoop = true;
+
+				while(runLoop) {
+					String state;
+					try {
+						state = bufReader.readLine();
+
+						if(state != null) {
+							if(!state.isEmpty()) {
+								processResponse(state);
+							}
+						}
+					}
+					catch(Exception e) {
+						runLoop = false;
+						logInfo("!! Error Reading from bufReader !!");
+						e.printStackTrace();
+					}
+
+					if(!isReady) {
+						if(!waitingForReady) {
+							sendCommand("isready", true);
+							logInfo("Waiting for engine ready");
+							waitingForReady = true;
+						}
+					}
+
+					pushCommands();
+				}
+
+				return null;
+			}
+		};
+		new Thread(readerLoop).start();
+
+		logInfo("...Initialized");
+
+		requestCommand("uci", true);
 	}
 
-	private void logInfo(String input) {
+	public void startNewGame() {
+
+		requestCommand("ucinewgame", true);
+	}
+
+	private void processResponse(String in) {
+
+		boolean consumed = false;
+
+		for(ResponseListener rl : responseListeners) {
+			if(rl.onResponse(in)) {
+				consumed = true;
+				break;
+			}
+		}
+
+		if(!consumed)
+			logInfo("eng < " + in);
+	}
+
+	/**
+	 * Send a command to the chess engine
+	 * @param cmd command to be sent
+	 */
+	public void requestCommand(String cmd, boolean flush) {
+		pushCmdList.add(new EngineCommand(cmd, flush));
+	}
+
+	private void sendCommand(String cmd, boolean flush) {
+		logInfo("cmd > " + cmd);
+		bufWriter.println(cmd);
+		if(flush)
+			flushWriter();
+	}
+
+	private void pushCommands() {
+
+		if(isReady) {
+			if(!pushCmdList.isEmpty()) {
+
+				for(EngineCommand cmd : pushCmdList)
+					sendCommand(cmd.getCommand(), cmd.shouldFlush());
+
+				pushCmdList.clear();
+
+				isReady = false;
+			}
+		}
+	}
+
+	public void logInfo(String input) {
 
 		if(logBuffer != null) {
 			if (isLogging()) {
@@ -67,7 +233,7 @@ public class ChessEngine {
 				LocalDateTime curDateTime = LocalDateTime.now();
 				String time = curDateTime.format(DateTimeFormatter.ISO_LOCAL_TIME);
 
-				String logLine = time + " | " + input;
+				String logLine = time.substring(0,10) + " | " + input;
 
 				System.out.println(logLine);
 
@@ -78,37 +244,15 @@ public class ChessEngine {
 	}
 
 	/**
-	 * Send a command to the chess engine
-	 * @param cmd command to be sent
-	 */
-	public void sendCommand(String cmd) {
-		logInfo("cmd > " + cmd);
-		bufWriter.println(cmd);
-	}
-
-	public String readResponse() {
-
-		String state = "DEBUG | uninitialized";
-
-		//logInfo("Attempting to read from bufReader");
-		try {
-			state = bufReader.readLine();
-			logInfo(state);
-		}
-		catch (IOException e) {
-			logInfo("!! Error Reading from bufReader !!");
-			e.printStackTrace();
-		}
-
-		return state;
-	}
-
-	/**
 	 * Flushes the BufferedWriter
 	 */
-	public void flushWriter() {
-		logInfo("Flushing Writer...");
+	private void flushWriter() {
+		logInfo("> > Flushing Writer...");
 		bufWriter.flush();
-		logInfo("Flush finished.");
+		logInfo("< < Flush finished.");
+	}
+
+	public String toString() {
+		return this.engineName;
 	}
 }
