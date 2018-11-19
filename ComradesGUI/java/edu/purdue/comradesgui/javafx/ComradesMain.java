cb154024507952c144efd7514cd569dcc81b85ff
thedavdo <david.baker542@gmail.com>
2018-11-19 14:44:55 -0500

    Main Program to be separate from FX Window

---
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/ComradesMain.java
@@ -0,0 +1,35 @@
+package edu.purdue.comradesgui.javafx;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class ComradesMain {
+
+
+
+	private ChessGame currentGame;
+
+	private ObservableList<Player> playerList;
+
+	public ComradesMain() {
+
+		playerList = FXCollections.observableArrayList();
+		currentGame = new ChessGame();
+	}
+
+	public ObservableList<Player> getPlayerList() {
+		return playerList;
+	}
+
+	public void addPlayer(Player ply) {
+		playerList.add(ply);
+	}
+
+	public ChessGame getCurrentGame() {
+		return currentGame;
+	}
+
+	public void startNewGame() {
+		currentGame.startGame();
+	}
+}
