eb75ab63a6bbb3dd42d37919f400892adcb0b40d
thedavdo <david.baker542@gmail.com>
2018-11-19 14:47:21 -0500

    Converted Characters to ChessCells

--- ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessGame.java
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessGame.java
@@ -2,7 +2,6 @@ package edu.purdue.comradesgui.javafx;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.effect.Light;
 
 public class ChessGame {
 
@@ -10,12 +9,12 @@ public class ChessGame {
 
 	private Player whitePlayer, blackPlayer;
 
-	private Character[][] arrayFEN;
-	private ObservableList<Character> deadWhite, deadBlack;
+	private ChessCell[][] chessCells;
+	private ObservableList<ChessPiece> deadWhite, deadBlack;
 
 	public ChessGame() {
 
-		setBoardFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+		setBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
 
 		deadBlack = FXCollections.observableArrayList();
 		deadWhite = FXCollections.observableArrayList();
@@ -41,43 +40,33 @@ public class ChessGame {
 		return blackPlayer;
 	}
 
+	public void startGame() {
+
+	}
+
 	public boolean addPiece(Character piece, int col, int row) {
 
-		if(arrayFEN != null) {
+		if(chessCells != null) {
 			if(Character.isAlphabetic(piece)) {
-				arrayFEN[col][row] = piece;
+				chessCells[col][row].setChessPiece(new ChessPiece(piece));
 			}
 		}
 
 		return false;
 	}
 
-	private int[][] parseMove(String move) {
-
-		int[][] moveArray = new int[2][2];
-
-		String pos1 = move.substring(0, 2);
-		String pos2 = move.substring(2);
-
-		moveArray[0][0] = Integer.parseInt("" + pos1.charAt(0), 16) - 10;
-		moveArray[0][1] = Integer.parseInt("" + pos1.charAt(1)) - 1;
-
-		moveArray[1][0] = Integer.parseInt("" + pos2.charAt(0), 16) - 10;
-		moveArray[1][1] = Integer.parseInt("" + pos2.charAt(1)) - 1;
-
-		return moveArray;
-	}
-
-	public boolean requestMove(String move) {
+	public boolean makeMove(String move) {
 
 		if(isMoveLegal(move)) {
-			int[][] moveArray = parseMove(move);
+			ChessMove moveParsed = new ChessMove(move, this);
 			if(move.length() > 3) {
 
-				Character piece = arrayFEN[moveArray[0][0]][moveArray[0][1]];
+				ChessCell fromCell = moveParsed.getFromCell();
+				ChessCell toCell = moveParsed.getToCell();
+				ChessPiece piece = fromCell.getChessPiece();
 				if(piece != null) {
-					arrayFEN[moveArray[0][0]][moveArray[0][1]] = null;
-					arrayFEN[moveArray[1][0]][moveArray[1][1]] = piece;
+					fromCell.setChessPiece(null);
+					toCell.setChessPiece(piece);
 				}
 			}
 		}
@@ -90,43 +79,19 @@ public class ChessGame {
 
 	public boolean isMoveLegal(String move) {
 
-		int[][] moveArray = parseMove(move);
-
-		int fromX = moveArray[0][0];
-		int fromY = moveArray[0][1];
-
-		int toX = moveArray[1][0];
-		int toY = moveArray[1][1];
-
-		Character curPiece = arrayFEN[fromX][fromY];
-		Character toPiece = arrayFEN[toX][toY];
+		ChessMove moveParsed = new ChessMove(move, this);
 
 
 		return true;
 	}
 
-	private int[][] getPieceMovements(Character piece) {
-
-		int[][] moves = null;
-
-		if(Character.toLowerCase(piece) == 'k') {
-			moves = new int[4][4];
-		}
-
-		return moves;
+	public ChessCell[][] getCells() {
+		return chessCells;
 	}
 
-	public boolean isMatchingTeam(Character piece1, Character piece2) {
-		return Character.isUpperCase(piece1) == Character.isUpperCase(piece2);
-	}
+	public void setBoardFromFEN(String strFEN) {
 
-	public Character[][] getArrayFEN() {
-		return arrayFEN;
-	}
-
-	public void setBoardFEN(String strFEN) {
-
-		Character[][] parsed = new Character[8][8];
+		ChessCell[][] parsed = new ChessCell[8][8];
 
 		String[] split = strFEN.split("/");
 
@@ -138,7 +103,8 @@ public class ChessGame {
 			int colIndex = 7;
 			for(Character in : unparsed) {
 				if(Character.isAlphabetic(in)) {
-					parsed[colIndex][rowIndex] = in;
+					parsed[colIndex][rowIndex] = new ChessCell(colIndex, rowIndex);
+					parsed[colIndex][rowIndex].setChessPiece(new ChessPiece(in));
 					colIndex--;
 				}
 				else if(Character.isDigit(in)) {
@@ -152,14 +118,14 @@ public class ChessGame {
 			rowIndex--;
 		}
 
-		arrayFEN = parsed;
+		chessCells = parsed;
 	}
 
 	public String getStringFEN() {
 
 		String boardFEN = null;
 
-		if(arrayFEN != null) {
+		if(chessCells != null) {
 			boardFEN = "";
 
 			for(int row = 7; row >= 0; row--) {
@@ -167,7 +133,7 @@ public class ChessGame {
 				int emptyCount = 0;
 				for(int col = 7; col >= 0; col--) {
 
-					Character loc = arrayFEN[col][row];
+					Character loc = chessCells[col][row].getChessPiece().getPieceChar();
 
 					if(loc != null) {
 						if(emptyCount > 0) {
