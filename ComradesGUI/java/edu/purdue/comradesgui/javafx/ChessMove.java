f07cbc853504b355c7be2f482a94eb841cdea93b
thedavdo <david.baker542@gmail.com>
2018-11-19 14:46:00 -0500

    Init ChessMove class

---
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessMove.java
@@ -0,0 +1,38 @@
+package edu.purdue.comradesgui.javafx;
+
+public class ChessMove {
+
+	private String rawMove;
+
+	private ChessGame chessGame;
+
+	private ChessCell fromCell;
+	private ChessCell toCell;
+
+	public ChessMove(String rawMove, ChessGame chessGame) {
+
+		this.chessGame = chessGame;
+		this.rawMove = rawMove;
+
+		String pos1 = rawMove.substring(0, 2);
+		String pos2 = rawMove.substring(2);
+
+		int fromCol = Integer.parseInt("" + pos1.charAt(0), 16) - 10;
+		int fromRow = Integer.parseInt("" + pos1.charAt(1)) - 1;
+
+		fromCell = chessGame.getCells()[fromCol][fromRow];
+
+		int toCol = Integer.parseInt("" + pos2.charAt(0), 16) - 10;
+		int toRow = Integer.parseInt("" + pos2.charAt(1)) - 1;
+
+		toCell = chessGame.getCells()[toCol][toRow];
+	}
+
+	public ChessCell getFromCell() {
+		return fromCell;
+	}
+
+	public ChessCell getToCell() {
+		return toCell;
+	}
+}
