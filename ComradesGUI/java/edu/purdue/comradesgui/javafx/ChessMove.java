2b1bd9999dffb83df06a70ce3267fe6f7aec7e19
thedavdo <david.baker542@gmail.com>
2018-11-20 20:21:40 -0500

    Fixed GridCoord to ArrayIndex OutOfBounds error

--- ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessMove.java
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessMove.java
@@ -17,15 +17,22 @@ public class ChessMove {
 		String pos1 = rawMove.substring(0, 2);
 		String pos2 = rawMove.substring(2);
 
-		int fromCol = Integer.parseInt("" + pos1.charAt(0), 16) - 10;
+		int fromCol = getNumFromLetter(pos1.charAt(0));//Integer.parseInt("" + pos1.charAt(0), 16) - 10;
 		int fromRow = Integer.parseInt("" + pos1.charAt(1)) - 1;
 
 		fromCell = chessGame.getCells()[fromCol][fromRow];
 
-		int toCol = Integer.parseInt("" + pos2.charAt(0), 16) - 10;
+		int toCol = getNumFromLetter(pos2.charAt(0));//Integer.parseInt("" + pos2.charAt(0), 16) - 10;
 		int toRow = Integer.parseInt("" + pos2.charAt(1)) - 1;
 
 		toCell = chessGame.getCells()[toCol][toRow];
+	}
+
+	private int getNumFromLetter(Character c) {
+
+		int num = (int) Character.toLowerCase(c) - 97;
+
+		return num;
 	}
 
 	public String getRawMove() {
