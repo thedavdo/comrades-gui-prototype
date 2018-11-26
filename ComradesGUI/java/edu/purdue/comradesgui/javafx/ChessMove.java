1095ec6df459af78376dc4a619245a7abf94a92b
thedavdo <david.baker542@gmail.com>
2018-11-25 23:22:58 -0500

    Added isMoveFound and getLeftOver methods

--- ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessMove.java
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/ChessMove.java
@@ -9,23 +9,30 @@ public class ChessMove {
 	private ChessCell fromCell;
 	private ChessCell toCell;
 
+	private String leftover;
+
 	public ChessMove(String rawMove, ChessGame chessGame) {
 
 		this.chessGame = chessGame;
 		this.rawMove = rawMove;
 
-		String pos1 = rawMove.substring(0, 2);
-		String pos2 = rawMove.substring(2);
+		if(isMoveFound()) {
 
-		int fromCol = getNumFromLetter(pos1.charAt(0));//Integer.parseInt("" + pos1.charAt(0), 16) - 10;
-		int fromRow = Integer.parseInt("" + pos1.charAt(1)) - 1;
+			String pos1 = rawMove.substring(0, 2);
+			String pos2 = rawMove.substring(2, 4);
 
-		fromCell = chessGame.getCells()[fromCol][fromRow];
+			leftover = rawMove.substring(4);
 
-		int toCol = getNumFromLetter(pos2.charAt(0));//Integer.parseInt("" + pos2.charAt(0), 16) - 10;
-		int toRow = Integer.parseInt("" + pos2.charAt(1)) - 1;
+			int fromCol = getNumFromLetter(pos1.charAt(0));//Integer.parseInt("" + pos1.charAt(0), 16) - 10;
+			int fromRow = Integer.parseInt("" + pos1.charAt(1)) - 1;
 
-		toCell = chessGame.getCells()[toCol][toRow];
+			fromCell = chessGame.getCells()[fromCol][fromRow];
+
+			int toCol = getNumFromLetter(pos2.charAt(0));//Integer.parseInt("" + pos2.charAt(0), 16) - 10;
+			int toRow = Integer.parseInt("" + pos2.charAt(1)) - 1;
+
+			toCell = chessGame.getCells()[toCol][toRow];
+		}
 	}
 
 	private int getNumFromLetter(Character c) {
@@ -35,8 +42,28 @@ public class ChessMove {
 		return num;
 	}
 
+	public boolean isMoveFound() {
+
+		boolean hasMove = true;
+
+		if(rawMove == null)
+			hasMove = false;
+
+		if(rawMove.isEmpty())
+			hasMove = false;
+
+		if(rawMove.equalsIgnoreCase("(none)"))
+			hasMove = false;
+
+		return hasMove;
+	}
+
 	public String getRawMove() {
 		return rawMove;
+	}
+
+	public String getLeftover() {
+		return leftover;
 	}
 
 	public ChessCell getFromCell() {
