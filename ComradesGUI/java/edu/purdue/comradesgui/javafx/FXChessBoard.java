078097867f413ba1491a8ff928e1dc407841f0b7
thedavdo <david.baker542@gmail.com>
2018-11-25 23:22:39 -0500

    Better Drawing of the Board

--- ComradesGUI/java/edu/purdue/comradesgui/javafx/FXChessBoard.java
+++ ComradesGUI/java/edu/purdue/comradesgui/javafx/FXChessBoard.java
@@ -18,9 +18,13 @@ public class FXChessBoard extends Canvas {
 
 	private Font fontChess;
 
-	public FXChessBoard(double boardSize, ChessGame chessGame) {
-		super(boardSize, boardSize);
+	private double boardSize;
+
+	public FXChessBoard(double containerSize, ChessGame chessGame) {
+		super(containerSize, containerSize);
 		this.chessGame = chessGame;
+
+		boardSize = containerSize * 0.94;
 
 		try {
 			FileInputStream fileInputStream = new FileInputStream(new File("MERIFONT.TTF"));
@@ -33,55 +37,90 @@ public class FXChessBoard extends Canvas {
 		animationTimer = new AnimationTimer() {
 			@Override
 			public void handle(long now) {
+				FXChessBoard.this.drawChessBoard();
+			}
+		};
+	}
 
-				GraphicsContext graphics = FXChessBoard.this.getGraphicsContext2D();
-				graphics.clearRect(0, 0, boardSize, boardSize);
+	public void drawChessBoard() {
 
-				ChessCell[][] cells = chessGame.getCells();
+		GraphicsContext graphics = getGraphicsContext2D();
+		graphics.clearRect(0, 0, getWidth(), getWidth());
 
-				for(int row = 7; row >= 0; row--) {
-					for(int col = 0; col < 8; col++) {
+		//graphics.setFill(Color.LIGHTGRAY);
+		//graphics.fillRect(0, 0, getWidth(), getWidth());
 
-						double xLoc = (7-col) * getCheckerSize();
-						double yLoc = (row + 1) * getCheckerSize();
+		ChessCell[][] cells = chessGame.getCells();
 
-						if((col + row) % 2 == 0)
-							graphics.setFill(Color.WHITE);
-						else
-							graphics.setFill(Color.LIGHTGRAY);
+		Color lightTile = new Color(1, 205d/255d, 160d/255d, 1);
+		Color darkTile = new Color(209d/255d, 140d/255d, 70d/255d, 1);
 
-						graphics.fillRect(xLoc, row *getCheckerSize(), getCheckerSize(), getCheckerSize());
+		for(int row = 0; row < 8; row++) {
 
-						ChessCell cell = cells[col][row];
+			double yLoc = getBoardPosY() + ((8 - row) * getCheckerSize());
+			for(int col = 0; col < 8; col++) {
 
-						if(cell.getChessPiece() != null) {
-							Character ch = cells[col][row].getChessPiece().getPieceChar();
+				double xLoc = getBoardPosX() + (col * getCheckerSize());
 
-							graphics.setFont(fontChess);
-							String str = "" + ch;
 
-							Text text = new Text(str);
-							text.setFont(fontChess);
+				if((col + row) % 2 == 0)
+					graphics.setFill(lightTile);
+				else
+					graphics.setFill(darkTile);
 
-							xLoc += ((( getCheckerSize()) - text.getLayoutBounds().getWidth()) / 2d) - 1d;
-							yLoc -= ((( getCheckerSize()) - text.getLayoutBounds().getHeight()) / 2d) + 1d;
+				graphics.fillRect(xLoc, yLoc - getCheckerSize(), getCheckerSize(), getCheckerSize());
 
-							if (Character.isUpperCase(ch)) {
-								graphics.setFill(Color.GRAY);
+				ChessCell cell = cells[col][row];
 
-								graphics.fillText(str, xLoc + 1, yLoc + 1);
-								graphics.setFill(Color.BEIGE);
-								graphics.fillText(str, xLoc, yLoc);
-							}
-							else {
-								graphics.setFill(Color.BLACK);
-								graphics.fillText(str, xLoc, yLoc);
-							}
-						}
+				if(cell.getChessPiece() != null) {
+					Character ch = cell.getChessPiece().getPieceChar();
+
+					graphics.setFont(fontChess);
+					String str = "" + ch;
+
+					Text text = new Text(str);
+					text.setFont(fontChess);
+
+					double pieceX = xLoc + (((getCheckerSize()) - text.getLayoutBounds().getWidth()) / 2d);
+					double pieceY = yLoc - (((getCheckerSize()) - text.getLayoutBounds().getHeight()) / 2d);
+
+					if (Character.isUpperCase(ch)) {
+						graphics.setFill(Color.GRAY);
+
+						graphics.fillText(str, pieceX + 1, pieceY + 1);
+						graphics.setFill(Color.BEIGE);
+						graphics.fillText(str, pieceX, pieceY);
+					}
+					else {
+						graphics.setFill(Color.BLACK);
+						graphics.fillText(str, pieceX, pieceY);
 					}
 				}
 			}
-		};
+
+			Font gridFont = Font.font("Consolas", 18);
+			Text text = new Text();
+			text.setFont(gridFont);
+
+			graphics.setFont(gridFont);
+			graphics.setFill(Color.BLACK);
+
+			String rowNum = "" + (row + 1);
+
+			text.setText(rowNum);
+			double rowX = getBoardPosX() - getCheckerSize()/3;
+			double rowY = yLoc - (((getCheckerSize()) - text.getLayoutBounds().getHeight()) / 2d);
+
+			graphics.fillText(rowNum, rowX, rowY);
+
+			String colNum = "" + String.valueOf((char) (row + 97));
+
+			text.setText(colNum);
+			double colX = (getContainerSize() - yLoc) + (((getCheckerSize()) - text.getLayoutBounds().getWidth()) / 2d);
+			double colY = getBoardPosY() + getBoardSize() + getCheckerSize()/3;
+
+			graphics.fillText(colNum, colX, colY);
+		}
 	}
 
 	public AnimationTimer getAnimationTimer() {
@@ -96,11 +135,24 @@ public class FXChessBoard extends Canvas {
 		return chessGame;
 	}
 
-	public double getBoardSize(){
+	public double getBoardPosX() {
+
+		return getContainerSize() - getBoardSize();//(getContainerSize() - boardSize) / 2;
+	}
+
+	public double getBoardPosY() {
+		return 0;//(getContainerSize() - boardSize) / 2;
+	}
+
+	public double getBoardSize() {
+		return boardSize;
+	}
+
+	public double getContainerSize(){
 		return this.getWidth();
 	}
 
 	public double getCheckerSize() {
-		return (getBoardSize() / 8d);
+		return (boardSize / 8);
 	}
 }
