20329e001454df530287dd29444047dd6bdcd158
thedavdo <david.baker542@gmail.com>
2019-10-01 20:00:03 -0400

    init launcher class

---
+++ ComradesGUI/java/edu/purdue/comradesgui/src/javafx/FXLauncher.java
@@ -0,0 +1,10 @@
+package edu.purdue.comradesgui.src.javafx;
+
+import javafx.application.Application;
+
+public class FXLauncher {
+
+    public static void main(String[] args) {
+        Application.launch(FXComradesGUI.class, args);
+    }
+}
